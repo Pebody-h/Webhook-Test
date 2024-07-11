8989253fe5d3c817a22014879c567119143bb54c
Pebody-h <heiderl753@gmail.com>
2024-07-11 09:48:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const WebSocket = require('ws');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/webhook-endpoint', (req, res) => {
+    console.log('Webhook received:', req.body);
+
+    // Enviar la notificación a todos los clientes conectados
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(req.body));
+        }
+    });
+
+    res.sendStatus(200).json({ success: true, data: req.body }); // Responder OK
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ websocket-server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const WebSocketPort = process.env.WEBSOCKET_PORT || 8080;
+
+const wss = new WebSocket.Server({ port: WebSocketPort });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+        // Aquí puedes emitir eventos a todos los clientes conectados
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
